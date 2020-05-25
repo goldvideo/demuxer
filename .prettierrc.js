e5e9bce33aab2b31d41fb798da5d41723f92efee
gemxx <gems.xu@gmail.com>
2020-05-25 21:55:21 +0800

    style: 💄 change indentation

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,19 +2,19 @@
 // place it into the root directory of your project.
 // https://prettier.io/docs/en/options.html
 module.exports = {
-	printWidth: 120,
-	tabWidth: 4,
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	trailingComma: 'none',
-	arrowParens: 'always',
-	rangeStart: 0,
-	rangeEnd: Infinity,
-	bracketSpacing: true,
-	requirePragma: false,
-	insertPragma: false,
-	proseWrap: 'preserve',
-	htmlWhitespaceSensitivity: 'css',
-	endOfLine: 'auto'
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'always',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    bracketSpacing: true,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'auto'
 };
